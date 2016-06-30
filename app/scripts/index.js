f1a32bf0fa56e640c35152ab1bf9080f426d98f4
vierello <vierello@hotmail.com>
2016-06-30 14:30:41 -0400

    final commit

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -6,16 +6,3 @@ var router = require('./router');
 $(function(){
   Backbone.history.start();
 });
-
-
-// var React = require('react');
-// var ReactDOM = require('react-dom');
-//
-//
-// var AppComponent = require('./components/chat.jsx').AppComponent;
-// var MessageCollection = require('./models/messages.js').MessageCollection;
-//
-// ReactDOM.render(
-//   React.createElement(AppComponent, {collection: MessageCollection}),
-//   $('#container')[0]
-// );
--- app/scripts/models/messages.js
+++ app/scripts/models/messages.js
@@ -6,7 +6,7 @@ var Message = Backbone.Model.extend({
 
 var MessageCollection = Backbone.Collection.extend({
   model: Message,
-  url: 'http://tiny-lasagna-server.herokuapp.com/collections/messages ',
+  url: 'https://tiny-lasagna-server.herokuapp.com/collections/messages ',
   comparator: 'createdAt'
 });
 
